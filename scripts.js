6dfacd4b9e7d1cab6d62f5bc934a9f2f73f2b1d9
Dennis Fyrén <106529711+dennisfyren@users.noreply.github.com>
2026-02-22 08:40:01 +0100

    Worked on styiling and protocol behaviour

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 
-let contact = document.getElementById("#cat-contact");
 
 function PrintPage(){
     const originalContent = document.body.innerHTML;
@@ -9,14 +8,33 @@ function PrintPage(){
     window.print();
     document.body.innerHTML = originalContent;
 }
+let a = document.getElementById("contact");
+let b = document.getElementById("facility");
+let c = document.getElementById("other");
+
+a.classList.add("hide-content");
+b.classList.add("hide-content");
+c.classList.add("hide-content");
+a.classList.toggle("hide-content");
 
 function SelectCategory(category){
+
+    a.classList.remove("hide-content");
+    b.classList.remove("hide-content");
+    c.classList.remove("hide-content");
+    a.classList.add("hide-content");
+    b.classList.add("hide-content");
+    c.classList.add("hide-content");
+    
     switch(category){
         case 1: console.log("Kontakt")
+            a.classList.toggle("hide-content");
             break;
         case 2: console.log("Anläggning")
+            b.classList.toggle("hide-content");
             break;
         case 3: console.log("Övrigt")
+            c.classList.toggle("hide-content");
             break;
     }
 }

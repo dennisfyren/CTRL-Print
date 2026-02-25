a7170a81cb655b902a0aa578cf7cf38e12c6b6fd
Dennis Fyrén <106529711+dennisfyren@users.noreply.github.com>
2026-02-25 22:05:42 +0100

    Finished HTML Structure, form and first style for the fire alarm protocol.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,23 @@
 const sideNav = document.getElementById("sidenav");
 const menuButton = document.getElementById("menu-button");
 const hiddenLinks = document.getElementById("hidden-links");
+
+const categories = [document.getElementById("test-1"), 
+                    document.getElementById("test-2"), 
+                    document.getElementById("test-3"),
+                    document.getElementById("test-4"),
+                    document.getElementById("test-5"),
+                    document.getElementById("test-6"),
+                    document.getElementById("test-7"),
+                    document.getElementById("test-8"),
+                    document.getElementById("test-9")];
 let menuOpen = 0;
 let linksOpen = 0;
 
 hiddenLinks.classList.add("show-links");
-
+function pause (milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
 function ToggleSidenav() {
     if (menuOpen === 0){
         sideNav.classList.add("toggle-sidenav");
@@ -16,6 +28,14 @@ function ToggleSidenav() {
         menuButton.classList.remove("move-button");
         menuOpen = 0;
     }
+}
+async function ToggleInputGroup(categoryNumber) {
+    categories.forEach(element => {
+        element.classList.remove("show-input-group");
+    });
+    categories[categoryNumber].classList.add("show-input-group");
+    await pause(350);
+    categories[categoryNumber].scrollIntoView();
 }
 function ToggleLinks() {
     if (linksOpen === 0) {

f83bcecfa79b913e72667c56720bba15e72aab92
Dennis Fyrén <106529711+dennisfyren@users.noreply.github.com>
2026-06-21 20:00:24 +0200

    Added password reset warning. Changed style of login page for a better experience.

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,16 +2,22 @@ import React, { useState } from "react";
 import Button from "../../components/Button";
 import logo from "../../assets/logoPNG.png";
 import TextInput from "../../components/TextInput";
+import { CircleX, X } from "lucide-react";
 
 function Login({ onLogin, resetApp }) {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [open, setOpen] = useState(false);
 
   async function handleSubmit() {
     setError("");
     const success = await onLogin(password);
     if (!success) setError("Fel lösenord");
     setPassword("");
+  }
+
+  function handleOpen() {
+    setOpen((prev) => !prev);
   }
 
   return (
@@ -23,33 +29,58 @@ function Login({ onLogin, resetApp }) {
       ></img>
       <div className="flex rounded gap-4 justify-center">
         <div className="flex gap-6 relative items-start">
-          <div>
-            <TextInput
-              label={"Ange lösenord"}
-              className="dark:text-main-text "
-              handleChange={(e) => setPassword(e.target.value)}
-              value={password}
-              type="password"
-              onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
-            />
-            {error && (
-              <h1 className="text-red-400 mt-7 justify-self-center">{error}</h1>
-            )}
-          </div>
-          <div className="flex flex-col gap-4 items-end">
-            <Button
-              label={"Logga in"}
-              className="bg-blue-500 hover:bg-blue-600 h-10 w-40"
-              handleClick={handleSubmit}
-            />
-            <Button
-              label={"Återställ app"}
-              className="bg-red-500 hover:bg-red-600 h-10 w-40"
-              handleClick={() => resetApp()}
-            />
-          </div>
+          {!open && (
+            <div>
+              <TextInput
+                label={"Ange lösenord"}
+                className="dark:text-main-text "
+                handleChange={(e) => setPassword(e.target.value)}
+                value={password}
+                type="password"
+                onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
+              />
+              {error && (
+                <h1 className="text-red-400 mt-7 justify-self-center">
+                  {error}
+                </h1>
+              )}
+            </div>
+          )}
+          {open && (
+            <div className="flex flex-col items-center gap-6 p-2 h-32 w-130 bg-red-900 border border-red-500 rounded dark:text-main-text">
+              <X
+                className="absolute right-3 cursor-pointer"
+                size={24}
+                onClick={() => handleOpen()}
+              />
+              <p className="mt-4">VARNING! Detta kommer att radera all data.</p>
+              <Button
+                label={"Återställ app"}
+                className="bg-red-500 hover:bg-red-600 h-10 w-40"
+                handleClick={() => resetApp()}
+              />
+            </div>
+          )}
+          {!open && (
+            <div className="flex flex-col gap-4 items-end">
+              <Button
+                label={"Logga in"}
+                className="bg-blue-500 hover:bg-blue-600 h-10 w-40"
+                handleClick={handleSubmit}
+              />
+            </div>
+          )}
         </div>
       </div>
+      <p className="dark:text-main-text mt-4 cursor-default">
+        Problem med att logga in?{" "}
+        <span
+          className="italic cursor-pointer text-sm"
+          onClick={() => handleOpen()}
+        >
+          Klicka här för att återställa app.
+        </span>
+      </p>
     </div>
   );
 }

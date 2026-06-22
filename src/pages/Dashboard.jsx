a50bbe85ed83a99777606aa72097fad38edacec5
Dennis Fyrén <106529711+dennisfyren@users.noreply.github.com>
2026-06-23 00:08:52 +0200

    Changed starting page to Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -12,7 +12,7 @@ import Protocol from "./Protocol";
 import { useAuth } from "../hooks/useAuth";
 
 function Dashboard({ isDark, setIsDark, logout, onSet }) {
-  const [open, setOpen] = useState("fire");
+  const [open, setOpen] = useState("dashboard");
   const [userSettings, setUserSettings] = useLocalStorage("userSettings", {
     name: "",
     company: "",
